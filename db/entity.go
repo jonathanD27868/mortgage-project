475038b374b42e99b901b264223f6d72d967427e
jonathand27868 <jonathand27868@gmail.com>
2022-05-08 18:12:09 +0200

    move New() from service.go to db/entity.go

--- db/entity.go
+++ db/entity.go
@@ -1,5 +1,11 @@
 package db
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
 type DBConfig struct {
 	Engine   string
 	Server   string
@@ -7,4 +13,22 @@ type DBConfig struct {
 	User     string
 	Password string
 	Database string
+}
+
+func New(filename string) DBConfig {
+	config := DBConfig{}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("Error while opening the db's config file: %s", err.Error())
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalf("Error while decoding the db's JSON config file : %s", err.Error())
+	}
+
+	return config
 }
